ef012bf9e38fcb2315cd57e162c2649c8795f5ca
Flaxen <alexander.carlsson@live.se>
2025-07-12 02:20:32 +0200

    added map spawning and switching POF

--- map_spawner.gd
+++ map_spawner.gd
@@ -1,21 +1,54 @@
 extends Node3D
 
+signal redrawing_map
+signal timeout
 @export var map_block: PackedScene
+@export var map_block2: PackedScene
+
+var flipper = 0
 var current_map
 
+var timer
+
+func _on_timeout():
+	print("funk")
+	timer.stop()
+
+func _on_zone_testsignal():
+	if timer.is_stopped():
+		timer.start()
+		current_map.queue_free()
+		emit_signal("redrawing_map")
+		emit_signal("timeout")
+		current_map=spawn_new_map()
+	
+		
 func spawn_new_map():
-	var mb = map_block.instantiate()
+	var mb
+	if flipper == 0:
+		mb = map_block.instantiate()
+		flipper = 1
+	else:
+		mb = map_block2.instantiate()
+		flipper = 0
+		
 	self.add_child(mb)
 	print("spawned map " + mb.name)
 	
 	var door_zones = mb.get_child(0).get_children()
+	
+	for door_zone in door_zones:
+		print(door_zone)
+		door_zone.test_signal.connect(Callable(self, "_on_zone_testsignal"))
+		
 	return mb
 	
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	current_map = spawn_new_map()
+	timer = Timer.new()
+	add_child(timer)
+	timer.wait_time = 3.0
+	self.timer.timeout.connect(Callable(self, "_on_timeout"))
 	
 	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
